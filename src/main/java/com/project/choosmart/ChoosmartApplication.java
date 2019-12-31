d615b40eb8cd6d6670ecfda8c3d6355f4d2cb58f
NajiyaShaju <najiya.shajifrq@gmail.com>
2019-12-31 13:32:27 +0530

    first commit

---
+++ src/main/java/com/project/choosmart/ChoosmartApplication.java
@@ -0,0 +1,13 @@
+package com.project.choosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoosmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoosmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/choosmart/ChoosmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.choosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoosmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
